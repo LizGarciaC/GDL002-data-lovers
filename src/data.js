634d65eebee8b7a245c2c2d8bf452e01bea1e422
Iris <iaguiarg@hotmail.com>
2019-02-18 12:58:10 -0600

    Muestra los pokemon

--- src/data.js
+++ src/data.js
@@ -1,8 +1,14 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+// const example = () => {
+//   return 'example';
+// };
+//
+// window.example = example;
 
-window.example = example;
+window.data= {
+  getAllPokemon:()=>{
+    return window.POKEMON.pokemon;
+  }
+};

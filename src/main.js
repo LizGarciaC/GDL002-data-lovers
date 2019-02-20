d5d8383c4fca1af41e114791706d83c04d92603e
LizGarciaC <lunanobel@hotmail.com>
2019-02-19 22:15:27 -0600

    Se agrega la función para ocultar y mostrar pantallas, se ordena código

--- src/main.js
+++ src/main.js
@@ -1,37 +1,137 @@
 // El evento Load ejecuta el código que le sigue, cuando la página ha cargado toda (html, css ...)
-window.addEventListener('load', ()=> { 
+
+let PantallaPrincipal=document.getElementById("divPantallaPrincipal");
+let ListaPokemon=document.getElementById("divListaPokemon");
+let Pokedex=document.getElementById("divPokedex");
+
+showMorePokemon.addEventListener('click', ()=> {
+    PantallaPrincipal.style.display="none";
+    Pokedex.style.display="none";
+    ListaPokemon.style.display="block";
+
     let pokemonList; // Definición de variable para asignar el resultado de la función Obtener Pokemon
-    pokemonList=data.getAllPokemon(); // Se solicita al objeto data una función con el arreglo con todos los pokemon
-    let pokemonBox=document.getElementById("generalList"); //Se define variable para agregar datos en un div id=generaList
-    pokemonBox.innerHTML="Pokemon Región Kanto";
-    let htmlBox="";
-    for (let contPokemon=0; contPokemon<pokemonList.length; contPokemon++){
-        
-        htmlBox=htmlBox + 
+      pokemonList=data.getAllPokemon(); // Se solicita al objeto data una función con el arreglo con todos los pokemon
+      let pokemonBox=document.getElementById("generalList"); //Se define variable para agregar datos en un div id=generaList
+      // pokemonBox.innerHTML="Pokemon Región Kanto";
+      let htmlBox="";
+      for (let contPokemon=0; contPokemon<pokemonList.length; contPokemon++){
+  
+        htmlBox=htmlBox +
         "<span>" +
-             pokemonList[contPokemon].id + " " + pokemonList[contPokemon].name + 
-             "<img src='"+pokemonList[contPokemon].img+"'/>" +
+                   pokemonList[contPokemon].id + " " + pokemonList[contPokemon].name +
+                   "<img src='"+pokemonList[contPokemon].img+"'/>" +
         "</span>";
-        console.log(htmlBox);
-    
-    }
-pokemonBox.innerHTML=htmlBox;
-    
+              // console.log(htmlBox);
+      }
+  pokemonBox.innerHTML=htmlBox;
+  
+  
     pokemonList.forEach(pokemon => {  // Código para poner el dropDown ó lista desplegable del objeto
-    let dropDownId=document.getElementById("pokemonId");
-    var option = document.createElement("option");
-    option.text = pokemon.id;
-    dropDownId.add(option);     
-        
+      let dropDownId=document.getElementById("pokeList");
+      let option = document.createElement("option");
+      option.text = pokemon.id;
+      dropDownId.add(option);
+  
     });
+  
+  
+  });
+  
+  // Funcion para buscar el pokemon
 
-        pokemonList.forEach(pokemon => {  // Código para poner el dropDown ó lista desplegable del objeto
-        let dropDownName=document.getElementById("pokemonName");
-        var option = document.createElement("option");
-        option.text = pokemon.name;
-        dropDownName.add(option); // Agrega el nuevo elemento a la caja de selección ó dropDown
 
-        });
-    
+  //function getPokemon(data, condition){
+      let getPokemon=(data,condition) => {
+    for(let i=0; i<data.length; i++){
+      let pokemon = data[i];
+      if (pokemon.name == condition) {
+        printFirstData(pokemon);
+        // console.log(pokemon);
+        return pokemon.id;
+      }
+    }
+  }
+  
+  function searchPoke(){
 
-});
+    PantallaPrincipal.style.display="none";
+    Pokedex.style.display="block";
+    ListaPokemon.style.display="none";
+
+    console.log(document.getElementById("namePokemon").value);
+    getPokemon(POKEMON.pokemon, document.getElementById("namePokemon").value);
+    document.getElementById("namePokemon").value="";
+    // console.log(document.getElementById("namePokemon").value);
+  }
+  // se corrige la llamada al boton
+  document.getElementById("searchPokemon").addEventListener("click", searchPoke)
+  
+  
+  function printFirstData(pokemon){
+    document.getElementById("info").innerHTML=pokemon.name;
+    document.getElementById("info1").src=pokemon.img;
+  setattributes
+  }
+  // -----------------------------------------------------------
+  
+  // function getName(){
+  //   let result="";
+  //   let resultid="";
+  //   let resultimagen="";
+  //   let pokemonList; // Definición de variable para asignar el resultado de la función Obtener Pokemon
+  //   pokemonList=data.getAllPokemon(); // Se solicita al objeto data una función con el arreglo con todos los pokemon
+  //   resultimagen="img";
+  //   pokemonList[name]="";
+  //   console.log(pokemonList);
+  //
+  //   let namePokemon=document.getElementById("namePokemon").value;
+  //
+  //   console.log(namePokemon);
+  //   for(let i=0; i<pokemonList.length; i++){
+  //     console.log(pokemonList[i]);
+  //     if (pokemonList[i].name == namePokemon) {
+  //       result=pokemonList[i].name;
+  //       resultid=pokemonList[i].id;
+  //       resultimagen=pokemonList.img;
+  //       console.log(result);
+  //     }
+  //     let resultado=result + " " + resultid + " " + resultimagen;
+  //     console.log(resultado);
+  //     return resultado;
+  //   }
+  //
+  // }
+  //
+  // document.getElementById("searchBtn").addEventListener("click",getName);
+  
+  
+  
+  
+  
+  // -------------------------------- para las imagenes-----------------
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
